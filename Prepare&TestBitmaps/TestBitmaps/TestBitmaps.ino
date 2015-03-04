81e38589fc16bcef2e079f0c514c84eab5dbf2e9
Quazi Imran Ahmed <qiahmedmail@gmail.com>
2015-03-03 23:20:29 -0500

    Changed TestBitmaps Sketch
    ye

--- Prepare&TestBitmaps/TestBitmaps/TestBitmaps.ino
+++ Prepare&TestBitmaps/TestBitmaps/TestBitmaps.ino
@@ -1,53 +1,45 @@
+#include <Bridge.h>
+#include <FileIO.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
-#define PIN 12
-#include <FileIO.h>
-#include <Bridge.h>
+
+#define PIN 12// pin for contolling data pin in matrix
+
+
+int sdarray[64]; // Buffer array for bitmap rendering
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
 NEO_MATRIX_TOP   + NEO_MATRIX_LEFT +
 NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
 NEO_GRB            + NEO_KHZ800);
 
-int sd[64];
 
-void drawbitmap(int colorbuffer[] ){
-  
-  int bufferindex = 0;
-  for (int y = 7 ; y >= 0 ; y--){ //after the color array is built, they are pushed into this loop where it build the each pixel 
-    for (int x = 0 ; x < 8 ; x++){     //and displays according to the color array
-        matrix.drawPixel(x,y,colorbuffer[bufferindex]);
-        bufferindex++;
-        
-    }
- }
-matrix.show();
-}// END FUNCTION
 void SDbytes(char path[],int sdarray[]){
-  File w=FileSystem.open(path,FILE_READ);
-  for(int i = 0 ; i <64; i++){
-    int x;
-    x=w.read() << 8; // Extract the MSB of the 16-bit integer
-    x |= w.read(); // Append the LSB to the previous MSB
-    sdarray[i] = x;  
-  }//end forloop
-  w.close();
-}// end function
-void hug(char path[]){
-  SDbytes(path,sd);
-  for(int i = 0; i <= 255; i++){
-    matrix.setBrightness(i);
-    drawbitmap(sd);
-  
-  }
-    for(int i = 255; i>=0; i--){
-    matrix.setBrightness(i);
-    drawbitmap(sd);
-    delay(2);
-  
-  }
+File w=FileSystem.open(path,FILE_READ);
 
+  int x;
+for(int i = 0 ; i < 64; i++){
+  
+  x=w.read() << 8; // Extract the MSB of the 16-bit integer
+  x |= w.read(); // Append the LSB to the previous MSB
+  sdarray[i] = x;  
+}//end forloop
+w.close();
+
+}// end function
+
+void drawbitmap(int colorbuffer[] ) {
+
+  int bufferindex = 0;
+  for (int y = 7 ; y >= 0 ; y--) { //after the color array is built, they are pushed into this loop where it build the each pixel
+    for (int x = 0 ; x < 8 ; x++) {    //and displays according to the color array
+      matrix.drawPixel(x, y, colorbuffer[bufferindex]);
+      bufferindex++;
+
+    }
+  }
+  matrix.show();
 }
 
 void fetchNsketch(char* werd){
@@ -55,44 +47,68 @@ void fetchNsketch(char* werd){
   char* path =(char*)malloc(strlen(w)+strlen(werd)+1); /* make space for the new string (should check the return value ...) */
   strcpy(path, w); /* copy name into the new var, used strncpy to combat buffer overflow*/
   strcat(path, werd); /* add the extension */
-  SDbytes(path,sd);
-  
-  drawbitmap(sd);
+  SDbytes(path,sdarray);
+
+  drawbitmap(sdarray);
+
   free(path); // deallocate memory used for char array
-  delay(2000); // timer for next bitmap
+  delay(1000);
+
 }
+
+
 void setup() {
   // put your setup code here, to run once:
-    pinMode(13,OUTPUT);
-  digitalWrite(13,HIGH);
-  matrix.begin();
-  matrix.setBrightness(200);
-  matrix.fillScreen(0);
-  
-  Bridge.begin();
-  FileSystem.begin();
-  
-  digitalWrite(13,LOW);
+Bridge.begin();
+FileSystem.begin();
+Serial.begin(9600);
+matrix.begin();
+matrix.setBrightness(255);
+matrix.fillScreen(0);
+matrix.show();
+matrix.drawPixel(0,0,matrix.Color(0,255,255));
+matrix.show();
+delay(500);
+matrix.fillScreen(0);
+matrix.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  fetchNsketch("lookrskullfy");
-  //fetchNsketch("flamex102");
-  //fetchNsketch("wispx1");
-  fetchNsketch("pinetree");
-  fetchNsketch("food");
-  fetchNsketch("pirateship");
-  fetchNsketch("clouds");
-  fetchNsketch("lookrskullfy");
-  fetchNsketch("magicbunny");
-  fetchNsketch("emptyglass");
-  fetchNsketch("halfglass");
-  fetchNsketch("fullglass");
-  fetchNsketch("galleyship");
-  fetchNsketch("pirateship");
-  fetchNsketch("slimer");
-  fetchNsketch("pinetree");
+/*
+  // put your main code here, to run repeatedly:
+File w=FileSystem.open("/mnt/sda1/AirTech/www/DecodedSprites/clouds",FILE_READ);
+for(int i = 0 ; i < 64 ; i++){
+  int x;
+  x=w.read() ; //msb
+  x |= w.read() <<8; //lsb
+  sdarray[i] = x;
+}//end forloop
+w.close();
+*/
+fetchNsketch("apple");
+fetchNsketch("confuzzleskullfy");
+fetchNsketch("mountainview");
+fetchNsketch("flamex101");
+fetchNsketch("flamex102");
+fetchNsketch("food");
+fetchNsketch("emptyglass");
+fetchNsketch("halfglass");
+fetchNsketch("fullglass");
+fetchNsketch("manatwall");
+fetchNsketch("pinetree");
+fetchNsketch("slimer");
+fetchNsketch("wispx1");
+fetchNsketch("galleyship");
+fetchNsketch("looklskullfy");
+fetchNsketch("lookrskullfy");
+fetchNsketch("coolbird");
+fetchNsketch("lollerpop");
+fetchNsketch("magicbunny");
+fetchNsketch("ring");
+fetchNsketch("exclamaskullfy");
+fetchNsketch("smokestacks");
+fetchNsketch("xmastree");
+fetchNsketch("lul");
 
 }
