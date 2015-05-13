ce41d0228efff0b25761458c510ec648ff00b08f
shimmyjimmy <qiahmedmail@gmail.com>
2015-05-13 18:07:18 -0400

    fixing the list

--- FurryTech/MQTT.ino
+++ FurryTech/MQTT.ino
@@ -25,29 +25,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   /*
     For some reason, the number 10 cannot be parsed 
-  */  
-  if (number== 1){fetchNsketch("cloud",animode,11);}
-  if (number==2){fetchNsketch("kissy",animode,16);}
-  if (number==3){fetchNsketch("croc",animode,3);}
-  if(number==4){fetchNsketch("chesth",animode,12);}
-  if(number==5){
-      hug("circle1");
-    hug("circle2");
-    hug("circle3");}
-  if(number==6){fetchNsketch("link" ,1,0);}
-  if(number==7){fetchNsketch("food" ,1,0);}
-  if(number==8){fetchNsketch("fire" ,animode,2);}
-  if(number==9){fetchNsketch("epicface",animode,7);}
-  if(number==10){heart(3);}
-  if(number==11){fetchNsketch("monkey",1,0);}
-  if(number==12){heart(2);}
-  if(number==13){fetchNsketch("mario",1,0);}
-  if(number==14){fetchNsketch("let",animode,5);}
-  if(number==15){fetchNsketch("snowcloud",1,0);}
-  if(number==16){fetchNsketch("stormcloud",1,0);}
-  if(number==17){fetchNsketch("parrot",1,0);}
-  if(number==18){fetchNsketch("link",1,0);}
-  if(number==19){fetchNsketch("wg0face",1,0);}
+  */ 
+}
+  if(number==1){fetchNsketch("epicface",animode,7);} 
+  if(number==2){fetchNsketch("croc",animode,3);}
+  if(number==3){fetchNsketch("wg0face",1,0);}
+  if(number==4){hug("circle1");hug("circle2");hug("circle3");}
+  if(number==5){fetchNsketch("kissy",animode,16);}
+  if(number==6){fetchNsketch("cloud",animode,11);}
+  if(number==7){fetchNsketch("chesth",animode,12);
+  if(number==8){fetchNsketch("link" ,1,0);}
+  if(number==9){fetchNsketch("food" ,1,0);}
+  if(number==10){fetchNsketch("fire" ,animode,2);}
+  if(number==11){heart(3)}
+  if(number==12){fetchNsketch("monkey",1,0);}
+  if(number==13){heart(2);}
+  if(number==14){fetchNsketch("mario",1,0);}
+  if(number==15){fetchNsketch("let",animode,5);}
+  if(number==16){fetchNsketch("snowcloud",1,0);}
+  if(number==17){fetchNsketch("stormcloud",1,0);}
+  if(number==18){fetchNsketch("parrot",1,0);}
+  if(number==19){fetchNsketch("link",1,0);}
   if(number==20){fetchNsketch("calci",animode,3);}
   else{
     connection();
